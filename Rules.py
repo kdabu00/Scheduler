aa0ee39e20e52f975890c0af9012075e34c3b9f7
davidcdc <49540227+davidcdc@users.noreply.github.com>
2020-10-08 16:01:34 -0700

    Create Rules.py
    October 8, 2020
    @ 4:01pm
    by David Chin

---
+++ Rules.py
@@ -0,0 +1,18 @@
+"""
+Code for Rule Implementation
+Author: David Chin
+
+A program that has code to implement the rules/constaints for the TRIUMF scheduler
+"""
+
+#Below is pseudocode that could work well with a generator in schedule that yields experiments
+
+#first_experiment = schedule.get_next_experiment()
+#expected_target = experiment.target
+#ok = True
+#while ok:
+#  next_experiment = schedule.get_next_experiment()
+#  if next_experiment.target is not expected_target:
+#    ok = False
+#
+#return ok
