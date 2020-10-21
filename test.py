4437674dd148cf7ea785165c718fde7c0a79d194
Raj123Kaur <49548644+Raj123Kaur@users.noreply.github.com>
2020-10-21 12:19:27 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+import constraint
+
+
+"This is a file to unittest. Right now I just made a template"
+class Test(unittest.TestCase):
+    def myTest(self):
+        a = 1
+        self.assertEqual(a, 1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
