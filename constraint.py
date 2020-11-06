f745022832518c85a47b0f5ce9db87c82c84a826
Kevin J. Dabu <55458444+kdabu00@users.noreply.github.com>
2020-11-06 10:19:49 -0800

    Add files via upload

--- constraint.py
+++ constraint.py
@@ -8,27 +8,15 @@ include the actual column headings: 'Data', 'Exp. #', 'Facility' etc...
 THIS IS NOT THE SAME FOR THE REQUESTS
 """
 
-import getpass
-import os.path
 import pandas as pd
 import datetime
 import calendar
-import FileManager as fm
+from Schedule import Schedule
 
-def main():
+
+def run_check(schedule):
     """Main function"""
 
-    # gets the current logged in user's name
-    user = getpass.getuser()
-
-    # Grabs filename input from user
-    schedule_name = ask_file_names()
-
-    # I placed my excel files in C:\Users\USERNAME\Documents. Note: depending on where the file is *change*
-    schedule_path = os.path.join("C:\\Users", user, "Documents", schedule_name)
-
-    # assign the excel data frame to schedule variable
-    schedule = read_file(schedule_path)
     constraint_log_set = set()
     logs = (check_tb_length(schedule)[1], check_tb_start_time(schedule)[1], 
             check_integer_weeks(schedule)[1], check_target_station(schedule)[1], 
@@ -40,33 +28,17 @@ def main():
             check_ts_tm_alternates(schedule)[0], check_minimum_length_of_tb(schedule)[0])
     valid_schedule = all(bools_list)
 
-    # Outputs - Will probably be made into a separate function and saved for future use
-    print("-" * 100)
-    print("OVERVIEW OF SCHEDULE RULES: there are", get_number_of_unsatisfied_constraints(bools_list),"unsatisfied constraints")
-    print("-" * 100)
-    
-    if valid_schedule == True:
-        print("This is a valid schedule")
+    if valid_schedule:
+        return True
     else:
-        print("This schedule is not valid: ", constraint_log_set)
+        return False
 
-def read_file(path: str) -> object:
-    """Open excel schedule, displays contents turns excel file into a data frame: schedule"""
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_columns', None)
-    excel_file = pd.read_excel(path)  # first index: row, second index: column ex. schedule.values[0][0] == 'Date'
-    return excel_file
-
-
-def ask_file_names() -> object:
-    """Prompts user for file names for requests and schedules, then returns them"""
-    schedule_name = input("Input schedule file name (Schedule 138 Ancestor.xlsx): ")
-    return schedule_name
 
 def findDay(date): 
     """Find a weekdays of a date"""
     weekday = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S').weekday() 
     return (calendar.day_name[weekday]) 
+
 
 def get_target_block_set(schedule: object):
     """Generate a list of all target blocks"""
@@ -79,6 +51,7 @@ def get_target_block_set(schedule: object):
             target_block_set = list(dict.fromkeys(target_block_list))
     return target_block_set, target_block_list
 
+
 def get_ts_tm_combo(schedule: object):
     """Get the list of the Target Station/Target Module combination"""
     combo_list = list()
@@ -89,10 +62,11 @@ def get_ts_tm_combo(schedule: object):
             combo_list_2 = list(dict.fromkeys(combo_list))
     return combo_list, combo_list_2
 
+
 def get_number_of_unsatisfied_constraints(bools_list):
     num_unsatisfied_constraints = 0
     for i in range(len(bools_list)):
-        if bools_list[i] == False:
+        if not bools_list[i]:
             num_unsatisfied_constraints += 1
     return num_unsatisfied_constraints
             
@@ -111,6 +85,7 @@ def check_tb_start_time(schedule: object):
         constraint_log = 'Target blocks start and end on a Tuesday DAY shift'
     return valid_schedule, constraint_log
 
+
 def check_integer_weeks(schedule: object):
     """Checks rule #10 Each schedule has a fixed start date, and runs for a fixed integer number of weeks"""
     total_shifts_in_schedule = schedule.index.size
@@ -122,6 +97,7 @@ def check_integer_weeks(schedule: object):
         constraint_log = 'The schedule should be a fixed integer weeks'
 
     return valid_schedule, constraint_log
+
 
 def check_target_station(schedule: object):
     """Checks rule #1 The location of the Target Station and Target module at the schedule start is fixed"""
@@ -138,6 +114,7 @@ def check_target_station(schedule: object):
 
     return valid_schedule, constraint_log
 
+
 def check_ts_tm_alternates(schedule: object):
     """checks rule #2 if the combination of Target Station/Target Module alternates for each target block"""
     target_block_set = get_target_block_set(schedule)[0]
@@ -151,6 +128,7 @@ def check_ts_tm_alternates(schedule: object):
             valid_schedule = False
             constraint_log = 'The Target Station/Target Module combination should alternate for each target block'
     return valid_schedule, constraint_log
+
 
 def check_tb_length(schedule: object):
     """Checks rule #3 & #5 for the schedule except the final target block"""
@@ -169,6 +147,7 @@ def check_tb_length(schedule: object):
                 target_block_shifts = 0
     return valid_schedule, constraint_log
 
+
 def check_minimum_length_of_tb(schedule: object):
     """Checks rule #6 The minimum length of the final target block in a schedule is 2 weeks """
     target_block_list = get_target_block_set(schedule)[1]
@@ -184,6 +163,3 @@ def check_minimum_length_of_tb(schedule: object):
             target_block_shifts = 0
     return valid_schedule, constraint_log
 
-if __name__ == "__main__":
-    main()
-    
