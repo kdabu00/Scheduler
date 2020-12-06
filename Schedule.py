4d5d347416b18fd6e4e255797652a8a5618d93ae
Kevin J. Dabu <55458444+kdabu00@users.noreply.github.com>
2020-12-05 21:58:57 -0800

    updated @Leo

--- Schedule.py
+++ Schedule.py
@@ -1,10 +1,10 @@
-
 """
 Schedule.py
 Author: Kevin Dabu
 Date: OCT.29 2020
 This file contains the basic methods and attributes of a schedule for the TRIUMF Scheduler
 """
+import pandas as pd
 
 
 class Schedule:
@@ -21,6 +21,9 @@ class Schedule:
         self.parameters = None
         self.is_valid = False
 
+
+    # self.size = get_schedule_size(schedule)
+
     @property
     def file_name(self):
         return self.name + ".xlsx"
@@ -30,13 +33,13 @@ class Schedule:
         """Finds the number of different scheduled experiments, returns these experiments"""
         # Ignores values in Exp. # that are equal to Exp. #,
         # Test, Setup or empty, IGNORING Experiment S2000 *Filler exp*
-        self.schedule = self.schedule.loc[(self.schedule['I_Exp.#'] != '') & (self.schedule['I_Exp.#'] != 'Test')
-                                & (self.schedule['I_Exp.#'] != 'Setup') & (self.schedule['I_Exp.#'] != 'S2000')]
+        self.schedule = self.schedule.loc[(self.schedule['I_Exp.#'] != '') & (self.schedule['I_Exp.#'] != 'TEST')
+                                          & (self.schedule['I_Exp.#'] != 'SETUP') & (
+                                                      self.schedule['I_Exp.#'] != 'S2000')]
 
         # Make a list of each row with ISAC experiment #, Facility, Target, and Source
         unique_exp = self.schedule[['I_Exp.#', 'I_Facility', 'I_Tgt', 'I_Source']].values.tolist()
         scheduled_exp = set()
-
         # Store each experiment as a tuple inside of a set
         for exp in unique_exp:
             scheduled_exp.add(tuple(exp))
@@ -45,8 +48,9 @@ class Schedule:
     @property
     def facilities(self):
         """Returns all ISAC facilities and amount of times they are scheduled"""
-        self.schedule = self.schedule.loc[(self.schedule['I_Exp.#'] != '') & (self.schedule['I_Exp.#'] != 'Test')
-                                & (self.schedule['I_Exp.#'] != 'Setup') & (self.schedule['I_Exp.#'] != 'S2000')]
+        self.schedule = self.schedule.loc[(self.schedule['I_Exp.#'] != '') & (self.schedule['I_Exp.#'] != 'TEST')
+                                          & (self.schedule['I_Exp.#'] != 'SETUP') & (
+                                                      self.schedule['I_Exp.#'] != 'S2000')]
         facilities = set(self.schedule['I_Facility'].tolist())
         return facilities
 
@@ -55,7 +59,8 @@ class Schedule:
         """Returns all available shifts"""
         self.schedule = self.schedule.loc[
             (self.schedule['Cyclotron_Offline'] != 'Shutdown') & (self.schedule['Cyclotron_Offline'] != 'Maintenance') &
-            (self.schedule['Cyclotron_Offline'] != 'Beam Development') & (self.schedule['Cyclotron_Offline'] != 'Mini Shutdown') &
+            (self.schedule['Cyclotron_Offline'] != 'Beam Development') & (
+                        self.schedule['Cyclotron_Offline'] != 'Mini Shutdown') &
             (self.schedule['BL2A_Offline'] != 'Maintenance') & (self.schedule['BL2A_Offline'] != 'Startup')]
         return self.schedule.index.size
 
@@ -63,13 +68,42 @@ class Schedule:
     def isvalid(self):
         return self.is_valid
 
+    @property
+    def date(self):
+        return self.schedule['Date'].tolist()
+
+    @property
+    def target(self):
+        return self.schedule['I_Tgt'].tolist()
+
+    @property
+    def source(self):
+        return self.schedule['I_Source'].tolist()
+
+    @property
+    def module(self):
+        return self.schedule['I_Mod'].tolist()
+
+    @property
+    def station(self):
+        return self.schedule['I_West/East'].tolist()
+
+    @property
+    def shift(self):
+        return self.schedule['Shift'].tolist()
+
+    @property
+    def size(self):
+        size = len(self.schedule.index)
+        return size
+
     def set_fitness_parameters(self, total_exp, priority, requests, fields, acc, fac):
         self.parameters = {'total_exp': total_exp,
-                            'priority': priority,
-                            'requests': requests,
-                            'fields': fields,
-                            'acc': acc,
-                            'fac': fac}
+                           'priority': priority,
+                           'requests': requests,
+                           'fields': fields,
+                           'acc': acc,
+                           'fac': fac}
 
     def set_priorities(self, var):
         self.priorities = var
@@ -94,6 +128,13 @@ class Schedule:
         return self.name
 
 
+"""
+def get_schedule_size(schedule):
+    size = schedule.index.size
+    return size
+"""
+
+
 def refactor_schedule(schedule):
     """Adjusts schedule columns to proper titles, ignores empty cells, fixes SIS/RILIS to just RILIS"""
     # Rename the column names to the appropriate values
@@ -107,7 +148,10 @@ def refactor_schedule(schedule):
     schedule = schedule.fillna('')
     # Change SIS/RILIS to just RILIS
     schedule['I_Source'] = schedule['I_Source'].replace(['SIS/RILIS'], 'RILIS')
+    schedule.loc[:, 'I_Source'] = schedule.loc[:, 'I_Source'].str.upper()
+    schedule.loc[:, 'I_Facility'] = schedule.loc[:, 'I_Facility'].str.upper()
+    schedule.loc[:, 'I_Exp.#'] = schedule.loc[:, 'I_Exp.#'].str.upper()
+    schedule.loc[:, 'I_Tgt'] = schedule.loc[:, 'I_Tgt'].str.upper()
     # Strip trailing and leading whitespace from facility column
     schedule['I_Facility'] = schedule['I_Facility'].str.strip()
     return schedule
-
--- constraint.py
+++ constraint.py
@@ -13,54 +13,94 @@ import datetime
 import calendar
 from Schedule import Schedule
 
+"""
+   p1 = Target block length
+   p2 = Target block start on Tuesady day shift
+   p3 = Schedule with integer weeks
+   p4 = Target Station and Target module at the schedule start is fixed
+   p5 = Target Station/Target Module alternates for each target block
+   p6 = The minimum length of the final target block in a schedule is 2 weeks
+"""
+
+p1 = True
+p2 = True
+p3 = True
+p4 = True
+p5 = True
+p6 = True
 
 def run_check(schedule):
-    """Main function"""
-
+    """Main function
     constraint_log_set = set()
-
-    logs = (check_tb_length(schedule.schedule)[1], check_tb_start_time(schedule.schedule)[1], 
-            check_integer_weeks(schedule.schedule)[1], check_target_station(schedule.schedule)[1], 
-            check_ts_tm_alternates(schedule.schedule)[1], check_minimum_length_of_tb(schedule.schedule)[1])
+    logs = (check_tb_length(schedule)[1], check_tb_start_time(schedule)[1],
+            check_integer_weeks(schedule)[1], check_target_station(schedule)[1],
+            check_ts_tm_alternates(schedule)[1], check_minimum_length_of_tb(schedule)[1])
     constraint_log_set.add(logs)
-
-    bools_list = (check_tb_length(schedule.schedule)[0], check_tb_start_time(schedule.schedule)[0], 
-            check_integer_weeks(schedule.schedule)[0], check_target_station(schedule.schedule)[0], 
-            check_ts_tm_alternates(schedule.schedule)[0], check_minimum_length_of_tb(schedule.schedule)[0])
-
+    bools_list = (check_tb_length(schedule)[0], check_tb_start_time(schedule)[0],
+            check_integer_weeks(schedule)[0], check_target_station(schedule)[0],
+            check_ts_tm_alternates(schedule)[0], check_minimum_length_of_tb(schedule)[0])
     valid_schedule = all(bools_list)
-
     if valid_schedule:
         return True
     else:
         return False
+    """
+    constraint_output = check_schedule(schedule)
+    return constraint_output
 
 
-def findDay(date): 
+
+def check_schedule(schedule):
+    logs = ''
+    bools = []
+
+    if p1:
+        logs += check_tb_length(schedule)[1] + '\n'
+        bools.append(check_tb_length(schedule)[0])
+    if p2:
+        logs += check_tb_start_time(schedule)[1] + '\n'
+        bools.append(check_tb_start_time(schedule)[0])
+    if p3:
+        logs += check_integer_weeks(schedule)[1] + '\n'
+        bools.append(check_integer_weeks(schedule)[0])
+    if p4:
+        logs += check_target_station(schedule)[1] + '\n'
+        bools.append(check_target_station(schedule)[0])
+    if p5:
+        logs += check_ts_tm_alternates(schedule)[1] + '\n'
+        bools.append(check_ts_tm_alternates(schedule)[0])
+    if p6:
+        logs += check_minimum_length_of_tb(schedule)[1] + '\n'
+        bools.append(check_minimum_length_of_tb(schedule)[0])
+
+    valid_schedule = all(bools)
+    return logs, valid_schedule
+
+
+def findDay(date):
     """Find a weekdays of a date"""
-    weekday = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S').weekday() 
-    return (calendar.day_name[weekday]) 
-
+    weekday = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S').weekday()
+    return (calendar.day_name[weekday])
 
 
 def get_target_block_set(schedule):
     """Generate a list of all target blocks"""
     target_block_list = list()
-    for i in range(schedule.schedule.index.size):  # schedule.index.size gets the amount of rows within the excel file
-        if (schedule.schedule.values[i][11] != "Tgt") and (pd.notnull(schedule.schedule.values[i][11])):
+    for i in range(len(schedule.schedule.index)):  # schedule.index.size gets the amount of rows within the excel file
+        if schedule.target[i] != '':
             # Ignores values in Tgt that are equal Tgt or empty
-            target_block = schedule.schedule.values[i][11]+schedule.schedule.values[i][12]+str(schedule.schedule.values[i][13])
-
+            target_block = schedule.target[i]+schedule.source[i]+str(schedule.module[i])
             target_block_list.append(target_block)
             target_block_set = list(dict.fromkeys(target_block_list))
     return target_block_set, target_block_list
 
+
 def get_ts_tm_combo(schedule):
     """Get the list of the Target Station/Target Module combination"""
     combo_list = list()
-    for i in range(schedule.schedule.index.size):
-        if (schedule.schedule.values[i][8] != "West / East") and (pd.notnull(schedule.schedule.values[i][8])):
-            combo = schedule.schedule.values[i][8] + str(schedule.schedule.values[i][13])
+    for i in range(len(schedule.schedule.index)):
+        if schedule.station[i] != '':
+            combo = schedule.station[i] + str(schedule.module[i])
             combo_list.append(combo)
             combo_list_2 = list(dict.fromkeys(combo_list))
     return combo_list, combo_list_2
@@ -72,14 +112,14 @@ def get_number_of_unsatisfied_constraints(bools_list):
         if not bools_list[i]:
             num_unsatisfied_constraints += 1
     return num_unsatisfied_constraints
-            
+
 
 def check_tb_start_time(schedule):
     """Checks rule #4 Target blocks start and end on a Tuesday DAY shift"""
-    start_date = str(schedule.schedule.values[2][0])
-    end_date = str(schedule.schedule.values[549][0])
-    start_shift = schedule.schedule.values[2][1]
-    end_shift = schedule.schedule.values[549][1]
+    start_date = str(schedule.date[2])
+    end_date = str(schedule.date[547])
+    start_shift = schedule.shift[2]
+    end_shift = schedule.shift[547]
     constraint_log = ""
     if findDay(start_date) == "Tuesday" and start_shift == "DAY" and findDay(end_date) == "Tuesday" and end_shift == "DAY":
         valid_schedule = True
@@ -91,8 +131,7 @@ def check_tb_start_time(schedule):
 
 def check_integer_weeks(schedule):
     """Checks rule #10 Each schedule has a fixed start date, and runs for a fixed integer number of weeks"""
-    total_shifts_in_schedule = schedule.schedule.index.size
-
+    total_shifts_in_schedule = schedule.size
     constraint_log = ""
     if (total_shifts_in_schedule-1) % 21 == 0:
         valid_schedule = True
@@ -101,6 +140,7 @@ def check_integer_weeks(schedule):
         constraint_log = 'The schedule should be a fixed integer weeks'
 
     return valid_schedule, constraint_log
+
 
 def check_target_station(schedule):
     """Checks rule #1 The location of the Target Station and Target module at the schedule start is fixed"""
@@ -138,16 +178,18 @@ def check_tb_length(schedule):
     target_block_list = get_target_block_set(schedule)[1]
     target_block_shifts = 0
     constraint_log = ""
-    for i in range(len(target_block_list)-43):  
+    for i in range(len(target_block_list)-43):
         if ('UCx' in target_block_list[i]) and (target_block_list[i] == target_block_list[i+1]):
             target_block_shifts += 1.25
         elif (target_block_list[i] == target_block_list[i+1]):
             target_block_shifts += 1
+        elif target_block_shifts < 63 or target_block_shifts > 105:
+            valid_schedule = False
+            constraint_log = 'The maximum length of a target block with UCx is 4 weeks, and other target block is 5 weeks. The minimum length of a target block is 3 weeks'
+            target_block_shifts = 0
         else:
-            if target_block_shifts < 63 or target_block_shifts > 105:
-                valid_schedule = False
-                constraint_log = 'The maximum length of a target block with UCx is 4 weeks, and other target block is 5 weeks. The minimum length of a target block is 3 weeks'
-                target_block_shifts = 0
+            valid_schedule = False
+
     return valid_schedule, constraint_log
 
 
@@ -157,7 +199,7 @@ def check_minimum_length_of_tb(schedule):
     target_block_shifts = 0
     constraint_log = ""
     for i in range(len(target_block_list)-1):
-        if (target_block_list[i] == target_block_list[i+1]):
+        if target_block_list[i] == target_block_list[i+1]:
             target_block_shifts += 1
         else:
             if target_block_shifts < 42 :
@@ -165,4 +207,3 @@ def check_minimum_length_of_tb(schedule):
                 constraint_log = 'The minimum length of the final target block in a schedule is 2 weeks'
             target_block_shifts = 0
     return valid_schedule, constraint_log
-
